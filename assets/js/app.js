9166418dbfc7ed5989998d0617fafcc465993560
boikoalexey <91904042+boikoalexey@users.noreply.github.com>
2021-10-23 09:21:57 +0300

    JS Modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
 $(function() {
+
     let intro = $("#intro");
     let header = $("#header");
     let introH = intro.innerHeight();
     let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
 
 
-    /* Header class on scroll */
-
+    /* Header class on scroll
+    =====================================*/
 
     headerScroll();
 
-    $(window).on("scroll resize", function() {
+    $(window).on("scroll  resize", function() {
         headerScroll();
     });
 
@@ -27,8 +29,10 @@ $(function() {
         }
     }
 
-    /* Smooth Scroll to section */
 
+
+    /* Smooth Scroll to sections
+    =====================================*/
 
     $("[data-scroll]").on("click", function(event) {
         event.preventDefault();
@@ -42,27 +46,88 @@ $(function() {
     });
 
 
-    /* ScrollSpy */
 
+
+    /* ScrollSpy
+    =====================================*/
     let windowH = $(window).height();
+    scrollSpy(scrollTop);
 
     $(window).on("scroll", function() {
-        let scrollTop = $(this).scrollTop();
-
-        $("[data-scrollspy]").each(function() {
-
-        let $this = $(this);
-        let sectionID = $this.data("scrollspy");
-        let sectionOffset = $this.offset().top;
-        sectionOffset = sectionOffset - (windowH / 2);
-
-        if(scrollTop >= sectionOffset) {
-            $('#nav [data-scroll').removeClass("active");
-
-            $('#nav [data-scroll="' + sectionID + '"]').addClass("active");
-        }
+        scrollTop = $(this).scrollTop();
+        scrollSpy(scrollTop);
     });
-});
+
+    function scrollSpy(scrollTop) {
+        $("[data-scrollspy]").each(function() {
+            let $this = $(this);
+            let sectionId = $this.data('scrollspy');
+            let sectionOffset = $this.offset().top;
+            sectionOffset = sectionOffset - (windowH * 0.33333);
+
+            if(scrollTop >= sectionOffset) {
+                $('#nav [data-scroll]').removeClass('active');
+                $('#nav [data-scroll="' + sectionId + '"]').addClass('active');
+            }
+
+            if(scrollTop == 0) {
+                $('#nav [data-scroll]').removeClass('active');
+            }
+        });
+    }
+
+
+
+    /* Modal
+    =====================================*/
+
+    $('[data-modal]').on('click', function(event) {
+        event.preventDefault();
+        let modal = $(this).data('modal');
+
+        $('body').addClass('no-scroll');
+        $(modal).addClass('show');
+
+        setTimeout(function() {
+            $(modal).find('.modal__content').css({
+                transform: 'scale(1)',
+                opacity: '1'
+            });
+        });
+    });
+
+
+    $('[data-modal-close]').on('click', function(event) {
+        event.preventDefault();
+        let modal = $(this).parents('.modal');
+        modalClose(modal);
+    });
+
+
+    $('.modal').on('click', function() {
+        let modal = $(this);
+        modalClose(modal);
+    });
+
+
+    $('.modal__content').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+
+    function modalClose(modal) {
+        modal.find('.modal__content').css({
+            transform: 'scale(0.5)',
+            opacity: '0'
+        });
+
+        setTimeout(function() {
+            $('body').removeClass('no-scroll');
+            modal.removeClass('show');
+        }, 200);
+    }
+
+
 
 
 });
