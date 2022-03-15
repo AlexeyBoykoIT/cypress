dd2d44692fda00e3e437fe4396daecababe815a3
Alexey Boyko <a.boyko@anviovr.com>
2022-03-15 10:50:47 +0300

    changes

---
+++ assets/js/domjs.js
@@ -0,0 +1,22 @@
+// const input = prompt('Enter');
+// const el = document.querySelector('.articles__text');
+
+// el.innerText = input;
+
+
+/* for (let i = 1; i <= 4; i++) {
+    let input = prompt('Enter');
+    let [i] = document.createElement('p');
+    [i].className = ('articles__text');
+    [i].innerText = input;
+    const messageBox = document.querySelector('.articles__item');
+    messageBox.append([i]);
+} */
+
+ /*    let input = prompt('Enter to text');
+    let i = document.createElement('p');
+    i.className = ('articles__text');
+    i.innerText = input;
+    const messageBox = document.querySelector('.articles__item');
+    messageBox.append(i); */
+
